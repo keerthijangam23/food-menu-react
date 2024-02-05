5e0746d54ed181b174e8cd18d43533afb04e8b24
keerthi <jangamkeerthi26@gmail.com>
2024-02-05 12:09:21 +0530

    add onClick event on card div

--- src/FoodCard.js
+++ src/FoodCard.js
@@ -1,7 +1,7 @@
 function FoodCard({card,onClick}) {
     return (
       <div className="container">
-        <div className="card" >
+        <div className="card" onClick={() => onClick(card)} >
           <img src={card.image} alt={card.foodName} />
           <h3>{card.foodName}</h3>
           <h2>Price: ${card.price}</h2>
